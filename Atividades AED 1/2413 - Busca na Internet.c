016b7e32d5edf57f2155727a3f3f7b169da74f3e
fnsantos-dev <fn.santos@unifesp.br>
2025-01-28 02:24:17 -0300

    28/01

---
+++ Atividades AED 1/2413 - Busca na Internet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    int primeiro_link = 0;
+    primeiro_link = t * 4;
+
+    printf("%d\n" , primeiro_link);
+}
